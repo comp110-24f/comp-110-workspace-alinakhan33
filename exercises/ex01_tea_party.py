dc1695e16b5b84642169fd4949da480f845c5358
alinakhan33 <alinak3@unc.edu>
2024-09-12 12:15:34 -0400

    Defining Main planner function, needs debugging

--- exercises/ex01_tea_party.py
+++ exercises/ex01_tea_party.py
@@ -1,8 +1,25 @@
-"""Writing a program to help me plan a tea party"""
+"""Writing a program to help me plan a tea party including the amount of guests and cost of supplies."""
 
 __author__ = "730485036"
 
 """Keep pushing changes to git!"""
+
+
+def main_planner(guests: int) -> None:
+    """This function brings every function below together"""
+    """Using variables to calculate # of tea bags and treats based on # of guests"""
+    tea_bag_num: int = tea_bags(people=guests)
+    treats_num: int = treats(people=guests)
+    """Now find the total cost of items"""
+    total: float = cost(tea_count=tea_bag_num, treat_count=treats_num)
+
+
+main_planner(guests=2)
+
+print("A Cozy Tea Party for " + str(guests) + "People!")
+print("Tea Bags: " + str(tea_bag_num))
+print("Treats: " + str(treats_num))
+print("Cost: $" + str(total_cost))
 
 
 def tea_bags(people: int) -> int:
@@ -16,7 +33,7 @@ tea_bags(people=2)
 def treats(people: int) -> int:
     """Num of treats is 1.5x the num of teas"""
     return int(tea_bags(people=people) * 1.5)
-    """The data type must return as int"""
+    """The data type must return as an int"""
 
 
 treats(people=2)
@@ -24,8 +41,9 @@ treats(people=2)
 
 def cost(tea_count: int, treat_count: int) -> float:
     """Calculate the cost of treats and tea bags combined, return type is float"""
-    teabag_cost = tea_count * 0.5
-    treat_cost = treat_count * 0.75
+    teabag_cost: float = tea_count * 0.5
+    treat_cost: float = treat_count * 0.75
+    """Use variables to create a set cost of 50 cents per tea bag and 75 cents per treat"""
     return teabag_cost + treat_cost
 
 
