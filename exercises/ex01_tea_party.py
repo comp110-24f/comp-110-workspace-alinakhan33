044ab1d1973c1b188a4f6c2bd8e74e657fb3190f
alinakhan33 <alinak3@unc.edu>
2024-09-12 10:08:27 -0400

    Defined tea bags function

---
+++ exercises/ex01_tea_party.py
@@ -0,0 +1,11 @@
+"""Writing a program to help me plan a tea party"""
+
+__author__ = "730485036"
+
+
+def tea_bags(people: int) -> int:
+    """How many tea bags we need based on num of people attending"""
+    return people * 2
+
+
+tea_bags(people=2)
