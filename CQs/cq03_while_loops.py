a653c25b3c5e1925a403f2b64f622a76324becdd
alinakhan33 <alinak3@unc.edu>
2024-09-18 20:56:38 -0400

    CQ03 While Loops

--- CQs/cq03_while_loops.py
+++ CQs/cq03_while_loops.py
@@ -1,3 +1,21 @@
 """Challenge Question #03 COMP110!"""
 
 __author__ = "730485036"
+
+
+def num_instances(phrase: str, search_char: str) -> int:
+
+    # increase this everytime an instance of search_char is found
+    count: int = 0
+
+    # track index so i can loop over every element of phrase
+    index: int = 0
+
+    # loop through every charater in the phrase
+    while index < len(phrase):
+
+        # check if the selected character matches search_char
+        if phrase[index] == search_char:
+            count = count + 1
+        index = index + 1
+    return count
