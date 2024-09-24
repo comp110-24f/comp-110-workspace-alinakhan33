2d0e48456663800096a4a701c66cbdbb8bce45e3
alinakhan33 <alinak3@unc.edu>
2024-09-24 19:46:29 -0400

    Part 4 complete and fixed

--- exercises/ex02_chardle.py
+++ exercises/ex02_chardle.py
@@ -8,48 +8,56 @@ __author__ = "730485036"
 # Prompting for an Input Word
 def input_word() -> str:
     # Gather user input
-    get_input: str = str(input("Enter a 5-character word: "))
+    word: str = str(input("Enter a 5-character word: "))
 
     # Create an if-else statement to test if the user input is 5 characters, if not, print an error msg
-    if len(get_input) == 5:
-        print(get_input)
+    if len(word) == 5:
+        print(word)
     else:
         print("Error: Word must contain 5 characters.")
 
-    return get_input
-
-
-input_word()
+    return word
 
 
 # Prompting for an Input Character
 def input_letter() -> str:
-    get_char_input: str = str(input(("Enter a single character: ")))
+    letter: str = str(input(("Enter a single character: ")))
 
     # Create an if-else statement to test if the user input is one character
-    if len(get_char_input) == 1:
-        print(get_char_input)
+    if len(letter) == 1:
+        print(letter)
     else:
         print("Error: You must enter exactly one character.")
-    return get_char_input
-
-
-input_letter()
+    return letter
 
 
 def contains_char(word: str, letter: str) -> None:
 
-    print("Searching for" + letter + "in" + word)
+    print("Searching for " + letter + " in " + word)
+
+    letter_instance: int = 0
+
+    # Filter thru each index to see if the character's in the word
     if word[0] == letter:
-        print(str(letter) + "found at index 0")
+        print(str(letter) + " found at index 0")
+        letter_instance += 1
     if word[1] == letter:
-        print(str(letter) + "found at index 1")
+        print(str(letter) + " found at index 1")
+        letter_instance += 1
     if word[2] == letter:
-        print(str(letter) + "found at index 2")
+        print(str(letter) + " found at index 2")
+        letter_instance += 1
     if word[3] == letter:
-        print(str(letter) + "found at index 3")
+        print(str(letter) + " found at index 3")
+        letter_instance += 1
     if word[4] == letter:
-        print(str(letter) + "found at index 4")
+        print(str(letter) + " found at index 4")
+        letter_instance += 1
+
+    if letter_instance > 0:
+        print(str(letter_instance) + " instances of " + letter + " found in " + word)
+    else:
+        print("No instances of " + letter + " found in " + word)
 
 
-contains_char(word="kitty", letter="z")
+contains_char(word=input_word(), letter=input_letter())
