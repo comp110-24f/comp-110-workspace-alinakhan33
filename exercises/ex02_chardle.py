a610fc03b092452e3740fea6d42db39232035326
alinakhan33 <alinak3@unc.edu>
2024-09-24 18:45:40 -0400

    Input letter if-else statement

--- exercises/ex02_chardle.py
+++ exercises/ex02_chardle.py
@@ -5,6 +5,7 @@ __author__ = "730485036"
 # Comment on code and push changes
 
 
+# Prompting for an Input Word
 def input_word() -> str:
     # Gather user input
     get_input: str = str(input("Enter a 5-character word: "))
@@ -19,3 +20,18 @@ def input_word() -> str:
 
 
 input_word()
+
+
+# Prompting for an Input Character
+def input_letter() -> str:
+    get_char_input: str = str(input(("Enter a single character: ")))
+
+    # Create an if-else statement to test if the user input is one character
+    if len(get_char_input) == 1:
+        print(get_char_input)
+    else:
+        print("Error: You must enter exactly one character.")
+    return get_char_input
+
+
+input_letter()
