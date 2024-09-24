6fa45fab14a1b58b4293c7207ad6b46368eba525
alinakhan33 <alinak3@unc.edu>
2024-09-24 18:27:22 -0400

    Get 5 character input

--- exercises/ex02_chardle.py
+++ exercises/ex02_chardle.py
@@ -1,3 +1,21 @@
 """EX02 - Chardle - A cute step toward Wordle."""
 
 __author__ = "730485036"
+
+# Comment on code and push changes
+
+
+def input_word() -> str:
+    # Gather user input
+    get_input: str = str(input("Enter a 5-character word: "))
+
+    # Create an if-else statement to test if the user input is 5 characters, if not, print an error msg
+    if len(get_input) == 5:
+        print(get_input)
+    else:
+        print("Error: Word must contain 5 characters.")
+
+    return get_input
+
+
+input_word()
