10650ab4b72cf59bf0ba49434eafccad53681125
alinakhan33 <alinak3@unc.edu>
2024-09-24 20:04:36 -0400

    Part 5 and 6

--- exercises/ex02_chardle.py
+++ exercises/ex02_chardle.py
@@ -5,59 +5,64 @@ __author__ = "730485036"
 # Comment on code and push changes
 
 
-# Prompting for an Input Word
-def input_word() -> str:
-    # Gather user input
-    word: str = str(input("Enter a 5-character word: "))
+def main() -> None:
 
-    # Create an if-else statement to test if the user input is 5 characters, if not, print an error msg
-    if len(word) == 5:
-        print(word)
-    else:
-        print("Error: Word must contain 5 characters.")
+    # Prompting for an Input Word
+    def input_word() -> str:
+        # Gather user input
+        word: str = str(input("Enter a 5-character word: "))
 
-    return word
+        # Create an if-else statement to test if the user input is 5 characters, if not, print an error msg
+        if len(word) == 5:
+            print(word)
+        else:
+            print("Error: Word must contain 5 characters.")
+            exit()
+        return word
+
+    # Prompting for an Input Character
+    def input_letter() -> str:
+        letter: str = str(input(("Enter a single character: ")))
+
+        # Create an if-else statement to test if the user input is one character
+        if len(letter) == 1:
+            print(letter)
+        else:
+            print("Error: You must enter exactly one character.")
+            exit()
+        return letter
+
+    def contains_char(word: str, letter: str) -> None:
+
+        print("Searching for " + letter + " in " + word)
+
+        letter_instance: int = 0
+
+        # Filter thru each index to see if the character's in the word
+        if word[0] == letter:
+            print(str(letter) + " found at index 0")
+            letter_instance += 1
+        if word[1] == letter:
+            print(str(letter) + " found at index 1")
+            letter_instance += 1
+        if word[2] == letter:
+            print(str(letter) + " found at index 2")
+            letter_instance += 1
+        if word[3] == letter:
+            print(str(letter) + " found at index 3")
+            letter_instance += 1
+        if word[4] == letter:
+            print(str(letter) + " found at index 4")
+            letter_instance += 1
+        # Print number of matches
+        if letter_instance > 0:
+            print(
+                str(letter_instance) + " instances of " + letter + " found in " + word
+            )
+        else:
+            print("No instances of " + letter + " found in " + word)
+
+    contains_char(word=input_word(), letter=input_letter())
 
 
-# Prompting for an Input Character
-def input_letter() -> str:
-    letter: str = str(input(("Enter a single character: ")))
-
-    # Create an if-else statement to test if the user input is one character
-    if len(letter) == 1:
-        print(letter)
-    else:
-        print("Error: You must enter exactly one character.")
-    return letter
-
-
-def contains_char(word: str, letter: str) -> None:
-
-    print("Searching for " + letter + " in " + word)
-
-    letter_instance: int = 0
-
-    # Filter thru each index to see if the character's in the word
-    if word[0] == letter:
-        print(str(letter) + " found at index 0")
-        letter_instance += 1
-    if word[1] == letter:
-        print(str(letter) + " found at index 1")
-        letter_instance += 1
-    if word[2] == letter:
-        print(str(letter) + " found at index 2")
-        letter_instance += 1
-    if word[3] == letter:
-        print(str(letter) + " found at index 3")
-        letter_instance += 1
-    if word[4] == letter:
-        print(str(letter) + " found at index 4")
-        letter_instance += 1
-
-    if letter_instance > 0:
-        print(str(letter_instance) + " instances of " + letter + " found in " + word)
-    else:
-        print("No instances of " + letter + " found in " + word)
-
-
-contains_char(word=input_word(), letter=input_letter())
+main()
