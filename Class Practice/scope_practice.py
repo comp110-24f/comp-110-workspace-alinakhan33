bf1b42a1d80ebc402ebfc0d258f7a50df59daaec
alinakhan33 <alinak3@unc.edu>
2024-09-26 20:58:00 -0400

    practice

--- Class Practice/scope_practice.py
+++ Class Practice/scope_practice.py
@@ -1,14 +1,17 @@
 def remove_chars(msg: str, char: str) -> str:
     """Return a copy of msg with all instances of char removed."""
-    copy: str = "" # set up empty str to copy values over
-    index: int: 0
+    copy: str = ""  # set up empty str to copy values over
+    index: int = 0
     while index < len(msg):
         # if the letter is NOT char
-        if not (msg[index]) != char: # msg[index] != char
-            # add it to copy
-            copy += msg[index] # copy = copy + msg[index]
-        index += 1 # index = index + 1
+        if not (msg[index]) == char:  # if msg[index] != char
+            copy += msg[index]  # copy = copy + msg[index]
+        index += 1
     return copy
 
 
-# remove_chars(msg="football", char="o") -> "ftball"
+word: str = "yoyo"  # GLOBAL variable
+
+print(remove_chars(word, char="y"))  # with keyword arguments
+
+# print(remove_chars(word, "o"))  with positional arguments
