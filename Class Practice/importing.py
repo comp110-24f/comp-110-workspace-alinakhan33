75e546886bf9eccd4db5b3163b1df8edfb4f38b7
alinakhan33 <alinak3@unc.edu>
2024-09-26 21:39:41 -0400

    class

---
+++ Class Practice/importing.py
@@ -0,0 +1,6 @@
+"""Practice importing"""
+
+from ClassPractice.scope_practice import remove_chars
+from ClassPractice.scope_practice import word
+
+print(word)
---
+++ Class Practice/scope_practice.py
@@ -0,0 +1,19 @@
+def remove_chars(msg: str, char: str) -> str:
+    """Return a copy of msg with all instances of char removed."""
+    copy: str = ""  # set up empty str to copy values over
+    index: int = 0
+    while index < len(msg):
+        # if the letter is NOT char
+        if not (msg[index]) == char:  # if msg[index] != char
+            copy += msg[index]  # copy = copy + msg[index]
+        index += 1
+    return copy
+
+
+if __name__ == "__main__":
+
+    word: str = "yoyo"  # GLOBAL variable
+
+    print(remove_chars(word, char="y"))  # with keyword arguments
+
+    # print(remove_chars(word, "o"))  with positional arguments
