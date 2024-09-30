4a26b25d1e19413f451044a27fb0891d17d8dd96
alinakhan33 <alinak3@unc.edu>
2024-09-30 14:55:27 -0400

    adding and removing from lists

--- cl13 practice.py
+++ cl13 practice.py
@@ -19,7 +19,31 @@ print(my_numbers)
 game_points: list[int] = [102, 86, 94]
 print(game_points)
 
+print(game_points[2])
+print(game_points[len(game_points) - 1])
+
 # Subscription Notation/Indexing
 print(game_points[2])
 last_game: int = game_points[2]
 print(last_game)
+
+# Modifying by Index
+# (because lists are mutable)
+game_points[1] = 72
+print(game_points)
+
+class: str = "110" # change it to "210"
+class_num[0] = "2"
+
+# Getting the length
+len(game_points)
+
+# Removing an Item
+game_points.pop(1)
+print(game_points)
+
+# Function name: display
+# Parameter: list of integers
+# RV: None
+# Print every element in the input list
+# Call display on game_points
