81d767310c5af1324e08aeee9466ccd50c627597
alinakhan33 <alinak3@unc.edu>
2024-09-30 14:50:49 -0400

    subscription notation

--- cl13 practice.py
+++ cl13 practice.py
@@ -14,3 +14,12 @@ print(my_numbers)
 my_numbers.append(1.5)
 my_numbers.append(2.3)
 print(my_numbers)
+
+# Creating an already populated list
+game_points: list[int] = [102, 86, 94]
+print(game_points)
+
+# Subscription Notation/Indexing
+print(game_points[2])
+last_game: int = game_points[2]
+print(last_game)
